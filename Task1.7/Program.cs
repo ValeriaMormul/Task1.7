e024462675730760d21d97b088c1a45710c81df0
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-02 01:38:01 +0200

    Добавьте файлы проекта.

---
+++ Task1.7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("3");
+        double celsius = double.Parse(Console.ReadLine());
+        double fahrenheit = ConvertCelsiusToFahrenheit(celsius);
+        Console.WriteLine($"Температура в градусах Фаренгейта: {fahrenheit} *F");
+    }
+    static double ConvertCelsiusToFahrenheit(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+}
